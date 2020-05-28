b66d29d305ce2b29ce604dc951bed14fcfaa1759
Jason Prince <jasoncprince@gmail.com>
2020-05-27 21:57:50 -0600

    fetching and loading images into components

---
+++ src/components/TagCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TagCard = ({ tag }) => {
+  return (
+    <>
+      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+        #{tag}
+      </span>
+    </>
+  );
+};
+
+export default TagCard;
